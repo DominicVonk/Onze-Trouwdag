aeece3e2e00915e2470c6fe2eaf26cce7c000936
Dominic Vonk <dominic@vonk.io>
2020-07-25 21:44:19 +0200

    Loaded

--- src/js/anim/writer.js
+++ src/js/anim/writer.js
@@ -1,26 +1,35 @@
 export default function writer(
   [element, htmlToDraw],
-  { charTime = 40, charsPerTime = 1 }
+  { charTime = 40, charsPerTime = 1 },
 ) {
-  return new Promise(function(resolve) {
+  return new Promise(function (resolve) {
     var chars = 0;
     var dt = +new Date();
-    window.requestAnimationFrame(function caller() {
-      var _dt = +new Date();
-      if (_dt - dt < charTime) {
-        window.requestAnimationFrame(caller);
-        return;
-      }
-      dt = _dt;
-      chars += charsPerTime;
-      if (chars <= htmlToDraw.length) {
-        element.innerHTML = htmlToDraw.substr(0, chars);
-        window.requestAnimationFrame(caller);
-      } else {
-        element.style.height = "auto";
-        element.style.width = "auto";
-        resolve();
-      }
-    });
+    if (!sessionStorage.loaded) {
+      window.requestAnimationFrame(function caller() {
+        var _dt = +new Date();
+        if (_dt - dt < charTime) {
+          window.requestAnimationFrame(caller);
+          return;
+        }
+        dt = _dt;
+        chars += charsPerTime;
+        if (chars <= htmlToDraw.length) {
+          element.innerHTML = htmlToDraw.substr(0, chars);
+          window.requestAnimationFrame(caller);
+        } else {
+          element.style.height = "auto";
+          element.style.width = "auto";
+          sessionStorage.loaded = 1;
+          resolve();
+        }
+      });
+    } else {
+      element.innerHTML = htmlToDraw;
+      element.style.height = "auto";
+      element.style.width = "auto";
+      sessionStorage.loaded = 1;
+      resolve();
+    }
   });
 }
